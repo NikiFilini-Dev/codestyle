c9c61491fc6ccef42aeed2b630b4aeb98875de63
lunavod <lunavod@bk.ru>
2022-04-06 16:52:55 +0300

    chore: versions bump

--- postinstall.js
+++ postinstall.js
@@ -5,5 +5,5 @@ const deps = Object.keys(packageJson.peerDependencies)
 
 console.log()
 console.log('\x1b[33m%s\x1b[0m', 'INSTALL DEPENDENCIES WITH:')
-console.log(`\x1b[2m%s\x1b[0m %s`, 'yarn add -D', deps.join(' '))
+console.log('\x1b[2m%s\x1b[0m %s', 'yarn add -D', deps.join(' '))
 console.log()
